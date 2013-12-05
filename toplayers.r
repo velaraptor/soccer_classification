eea30ce1bbb1c57d21ec13d81169773b12fb9535
velaraptor <christophvel@gmail.com>
2013-12-05 17:50:33 -0500

    Update toplayers.r

--- toplayers.r
+++ toplayers.r
@@ -5,10 +5,7 @@ top<-topplayers[,3]
 tenpremier<-read.csv("8_2010_features.csv")
 tenpremier<-tenpremier[,c(1:5,36:55)]
 tenpremier.1<-tenpremier[,c(6:length(tenpremier))]
-for(i in 1:length(tenpremier.1)){
-	j=i;
-	tenpremier.1[is.na(tenpremier.1)]<-0;
- }
+tenpremier.1[is.na(tenpremier.1)]<-0
 ten<-tenpremier[,1:5]
 fix.ten.premier<-cbind(ten,tenpremier.1)
 ggplot(fix.ten.premier,aes(x=num_goals,y=num_shots_on_target))+geom_point()+geom_text(aes(label=full_name),hjust=0, vjust=0,size=2)
