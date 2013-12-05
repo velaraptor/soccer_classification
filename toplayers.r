3f67ef6715ff88560b826abdd0f1ebbd19024f6c
velaraptor <christophvel@gmail.com>
2013-12-05 17:44:12 -0500

    Update toplayers.r

--- toplayers.r
+++ toplayers.r
@@ -10,7 +10,9 @@ for(i in 1:length(tenpremier.1)){
 	j=i;
 	tenpremier.1[is.na(tenpremier.1)]<-0;
  }
- ten<-tenpremier[,1:5]
- fix.ten.premier<-cbind(ten,tenpremier.1)
- ggplot(fix.ten.premier,aes(x=num_goals,y=num_shots_on_target))+geom_point()+geom_text(aes(label=full_name),hjust=0, vjust=0,size=2)
- ggplot(fix.ten.premier,aes(x=num_fouls_conceded,y=num_aerial_duels))+geom_point()+geom_text(aes(label=full_name),hjust=0, vjust=0,size=2)
+ten<-tenpremier[,1:5]
+fix.ten.premier<-cbind(ten,tenpremier.1)
+ggplot(fix.ten.premier,aes(x=num_goals,y=num_shots_on_target))+geom_point()+geom_text(aes(label=full_name),hjust=0, vjust=0,size=2)
+ggplot(fix.ten.premier,aes(x=num_fouls_conceded,y=num_aerial_duels))+geom_point()+geom_text(aes(label=full_name),hjust=0, vjust=0,size=2)
+ggplot(fix.ten.premier,aes(x=num_appearances,y=mins_played))+geom_point()+geom_text(aes(label=full_name),hjust=0, vjust=0,size=2)
+ggplot(fix.ten.premier,aes(x=num_appearances,y=num_touches))+geom_point()+geom_text(aes(label=full_name),hjust=0, vjust=0,size=2)
