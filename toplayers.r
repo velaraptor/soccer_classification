8095ea3be308e8f4feb7a025e6b5a6f838dcc6de
velaraptor <christophvel@gmail.com>
2013-12-07 16:58:36 -0500

    Update toplayers.r

--- toplayers.r
+++ toplayers.r
@@ -12,6 +12,8 @@ ggplot(fix.ten.premier,aes(x=num_goals,y=num_shots_on_target))+geom_point()+geom
 ggplot(fix.ten.premier,aes(x=num_fouls_conceded,y=num_aerial_duels))+geom_point()+geom_text(aes(label=full_name),hjust=0, vjust=0,size=2)
 ggplot(fix.ten.premier,aes(x=num_appearances,y=mins_played))+geom_point()+geom_text(aes(label=full_name),hjust=0, vjust=0,size=2)
 ggplot(fix.ten.premier,aes(x=num_appearances,y=num_touches))+geom_point()+geom_text(aes(label=full_name),hjust=0, vjust=0,size=2)
+##IM DOING 2010 First
+
 eight<-read.csv("8_2010_features.csv")
 twentyone<-read.csv("21_2010_features.csv")
 twentytwo<-read.csv("22_2010_features.csv")
@@ -31,37 +33,48 @@ fulll<-full[,1:14]
 fix.full<-cbind(fulll,full.1)
 kmean.full<-kmeans(fix.full[,c(2,15:length(fix.full))],centers=5)
 ggplot(fix.full,aes(x=position,y=num_touches))+geom_point(color=kmean.full$cluster,group=kmean.full$cluster)
-
 fix.full[fix.full$full_name %in% top,26]<-1
 colnames(fix.full)[26] <- "top"
 
 defender<-fix.full[fix.full$position==1,]
 mean.height<-mean(defender$height,na.rm=TRUE)
+mean.age<-mean(defender$age,na.rm=TRUE)
 mean.weight<-mean(defender$weight,na.rm=TRUE)
 defender[is.na(defender$height),3]<-mean.height
 defender[is.na(defender$weight),4]<-mean.weight
+defender[is.na(defender$age),5]<-mean.age
 
 forward<-fix.full[fix.full$position==2,]
 mean.height.2<-mean(forward$height,na.rm=TRUE)
 mean.weight.2<-mean(forward$weight,na.rm=TRUE)
+mean.age.2<-mean(forward$age,na.rm=TRUE)
 forward[is.na(forward$height),3]<-mean.height.2
 forward[is.na(forward$weight),4]<-mean.weight.2
+forward[is.na(forward$age),5]<-mean.age.2
 
 goal<-fix.full[fix.full$position==3,]
 mean.height.3<-mean(goal$height,na.rm=TRUE)
 mean.weight.3<-mean(goal$weight,na.rm=TRUE)
+mean.age.3<-mean(goal$age,na.rm=TRUE)
 goal[is.na(goal$height),3]<-mean.height.3
 goal[is.na(goal$weight),4]<-mean.weight.3
+goal[is.na(goal$age),5]<-mean.age.3
 
 mid<-fix.full[fix.full$position==4,]
 mean.height.4<-mean(mid$height,na.rm=TRUE)
 mean.weight.4<-mean(mid$weight,na.rm=TRUE)
+mean.age.4<-mean(mid$age,na.rm=TRUE)
 mid[is.na(mid$height),3]<-mean.height.4
 mid[is.na(mid$weight),4]<-mean.weight.4
+mid[is.na(mid$age),5]<-mean.age.4
 
 player<-fix.full[fix.full$position==5,]
 mean.height.5<-mean(player$height,na.rm=TRUE)
 mean.weight.5<-mean(player$weight,na.rm=TRUE)
+mean.age.5<-mean(player$age,na.rm=TRUE)
 player[is.na(player$height),3]<-mean.height.5
 player[is.na(player$weight),4]<-mean.weight.5
+player[is.na(player$age),5]<-mean.age.5
+
+
 fix.merge<-rbind.fill(defender,forward,goal,mid,player)
